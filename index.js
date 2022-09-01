a429a28047571dda5838cfa739bdf15bc06edde4
fzrsahi <fazrul.anugrah17@gmail.com>
2022-09-01 18:16:57 +0800

    add post method

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
 const express = require('express');
+const mysql = require('mysql');
+
+
+
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'monster88',
+  database: 'barang'
+});
 
 const app = express();
-
+app.use(express.urlencoded({extended: false}));
 app.use(express.static('public'));
 app.set('view engine','ejs');
 
@@ -9,7 +19,21 @@ app.get('/', (req, res) => {
   res.render('top.ejs');
 });
 app.get('/index', (req, res) => {
-  res.render('index.ejs');
+  connection.query('select * from items',(error,results)=> {
+    console.log(results);
+    res.render('index.ejs',{items:results });
+  })
 });
+app.get('/new',(req,res)=> {
+  res.render('new.ejs');
+})
 
+app.post('/create',(req,res) =>{
+  console.log(req.body.itemName);
+  connection.query('insert into items(name) values(?)',
+  [req.body.itemName],
+  (error,result)=>{
+    res.redirect('/index')
+  })
+})
 app.listen(3000);
